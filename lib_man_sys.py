a9aaf269b6080670494b90b4a8cd7c15bcc2c5f4
Okan KURTAR <dorokozmer@gmail.com>
2024-02-19 20:05:24 +0300

    update

--- lib_man_sys.py
+++ lib_man_sys.py
@@ -1,5 +1,5 @@
 import os
-
+# ANSI escape sequences
 class Colors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
@@ -12,16 +12,27 @@ class Colors:
 
 class Library:
     def __init__(self, filename='books.txt'):
+        """
+        Kütüphaneyi başlatır ve kitap listesi için bir dosya açar.
+
+        Parametreler:
+        filename (str): Kitapların saklanacağı dosyanın adı. Varsayılan değer 'books.txt'.
+        """
         self.file = open(filename, 'a+', encoding='utf-8')
         self.filename = filename
 
+    # Kütüphane nesnesi silindiğinde dosyanın kapatılmasını sağlamak için Destructor method kullanıyoruz.
     def __del__(self):
         self.file.close()
 
     def list_books(self):
-        self.file.seek(0)
+        """
+        Kütüphanedeki tüm kitapları listeler. Her kitabın adı ve yazarı ekrana yazdırılır.
+        Eğer kütüphanede hiç kitap yoksa, bir uyarı mesajı gösterilir.
+        """
+        self.file.seek(0) # dosyanın baştan okunması için
         books = self.file.read().splitlines()
-        if not books:
+        if not books: 
             print(Colors.WARNING + "No books found in the library." + Colors.ENDC)
         else:
             print(Colors.OKBLUE + "Listing all books:" + Colors.ENDC)
@@ -30,42 +41,54 @@ class Library:
                 print(f"{Colors.BOLD}Book Name:{Colors.ENDC} {name}, {Colors.BOLD}Author:{Colors.ENDC} {author}")
 
     def add_book(self):
+        """
+        Kullanıcıdan yeni bir kitap eklemek için gerekli bilgileri alır ve bu kitabı kütüphaneye ekler.
+        Kullanıcıdan kitabın adı, yazarı, yayın tarihi ve sayfa sayısı istenir.
+        Eğer aynı isimde bir kitap zaten mevcutsa, kullanıcıya uyarı verilir ve kitap eklenmez.
+        Kullanıcı kitabı eklemek istediğini onaylarsa, kitap kütüphaneye eklenir.
+        """
         name = input("Book name: ")
         author = input("Author: ")
-        release_date = input("Release date: ")  # Receive information from the user for publication date
-        pages = input("Number of pages: ")  # Get information from the user for the number of pages
+        release_date = input("Release date: ")  # Kitabın yayın tarihi
+        pages = input("Number of pages: ")  # Syfa sayısı
         
-        # Check if a book with the same name is already in the library
+        # Kütüphanede aynı isimde bir kitap olup olmadığını kontrol amaçlı;
         self.file.seek(0)
         books = self.file.read().splitlines()
         for book in books:
-            book_name, _, _, _ = book.split(', ', 3)  # Only extract the book name
-            if name.lower() == book_name.lower():  # Convert to lowercase for case-insensitive comparison
+            book_name, _, _, _ = book.split(', ', 3) 
+            if name.lower() == book_name.lower():  # hepsini küçük harf yaparak standartlaştırma
                 print(Colors.WARNING + "A book with the same name already exists in the library." + Colors.ENDC)
-                return  # If a book with the same name is found, exit the function
+                return  # Aynı isimde bir kitap bulunursa, fonksiyondan çıkılır
 
-        # Ask for confirmation before adding the book
+        # Bir kitap eklemeden emin olup olmadığını sor
         confirmation = input("Are you sure you want to add this book to the library? (y/n): ").lower()
         if confirmation == 'y':
-            # If confirmed, add the book to the file
+            # Onaylanırsa, kitabı dosyaya ekle
             self.file.write(f"{name}, {author}, {release_date}, {pages}\n")
-            self.file.flush()  # Ensure data is written to the file
+            self.file.flush()  # Verilerin dosyaya yazıldığından emin ol
             print(Colors.OKGREEN + "Book added successfully!" + Colors.ENDC)
         else:
             print(Colors.WARNING + "Book addition cancelled." + Colors.ENDC)
 
     def remove_book(self):
+        """
+        Kullanıcıdan kaldırılacak kitabın adını ister ve bu adı kullanarak kütüphanedeki kitapları arar.
+        Eğer eşleşen kitap bulunursa, kullanıcıdan kitabı kaldırmak için onay ister.
+        Onay verilirse, eşleşen kitap kütüphaneden kaldırılır.
+        Eğer eşleşen kitap bulunamazsa, kullanıcıya bir uyarı mesajı gösterilir.
+        """
         title_to_remove = input("Title of the book to remove: ").lower()
         self.file.seek(0)
         books = self.file.read().splitlines()
-        books_lower = [book.lower() for book in books]  # Kitapları küçük harfe çevir
+        books_lower = [book.lower() for book in books]
         matched_books = [book for book in books_lower if title_to_remove in book]
 
-        if matched_books:
+        if matched_books: # eşleşen kitap kontrolü
             confirmation = input(f"Are you sure you want to remove '{title_to_remove}' (y/n)? ").lower()
-            if confirmation == 'y':
+            if confirmation == 'y': # onay?
                 books = [book for book in books if book.lower() not in matched_books]
-                with open(self.filename, 'w', encoding='utf-8') as f:
+                with open(self.filename, 'w', encoding='utf-8') as f: # yazma modunda aç ve güncelle
                     for book in books:
                         f.write(book + '\n')
                 print(Colors.OKGREEN + "Book removed successfully!" + Colors.ENDC)
@@ -75,10 +98,16 @@ class Library:
             print(Colors.FAIL + "Book not found!" + Colors.ENDC)
 
     def search_books(self):
-        search_query = input("Enter book name or author to search: ").lower()  # Convert user query to lowercase
+        """
+        Kullanıcıdan bir kitap adı veya yazar ismi istenir.
+        Girilen sorgu ile eşleşen tüm kitaplar listelenir.
+        Eğer eşleşen kitaplar bulunursa, her birinin adı ve yazarı ekrana yazdırılır.
+        Eşleşen kitap bulunamazsa, kullanıcıya bir bilgi mesajı gösterilir.
+        """
+        search_query = input("Enter book name or author to search: ").lower()  # Sorguyu küçük harfe dönüştür.
         self.file.seek(0)
         books = self.file.read().splitlines()
-        found_books = [book for book in books if search_query in book.lower()]  # Lowercase the data in the file and search
+        found_books = [book for book in books if search_query in book.lower()]  # Verileri küçük harfe çevirip ara
         if found_books:
             print(Colors.OKBLUE + "Books found:" + Colors.ENDC)
             for book in found_books:
@@ -88,6 +117,14 @@ class Library:
             print(Colors.WARNING + "No books matched your search." + Colors.ENDC)
 
 def main_menu(lib):
+    """
+    Kullanıcıya bir ana menü sunar ve kullanıcıdan seçim yapmasını ister.
+    Kullanıcı listeleme, kitap ekleme, kitap kaldırma, kitap arama ve çıkış işlemlerinden birini seçebilir.
+    Kullanıcının seçimine bağlı olarak ilgili işlem gerçekleştirilir.
+
+    Parametreler:
+    lib (Library): Kütüphane sınıfının bir örneği
+    """
     while True:
         print("\n" + Colors.HEADER + "*** MENU ***" + Colors.ENDC)
         print("1) List Books\n2) Add Book\n3) Remove Book\n4) Search Books\nq) Exit")
